c257ae1b13f5147763ed3ffc871fff9509da5f47
স্নেহাংশু ফুকন <hello@snehanshu.com>
2023-11-11 12:26:13 +0530

    organize inside src folder

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+/** @param {import("@11ty/eleventy").UserConfig} config */
+module.exports = function (config) {
+  return {
+    dir: {
+      input: "src",
+    },
+  };
+};
--- src/index.md
+++ src/index.md
@@ -1,5 +1,5 @@
 ---
-layout: layout.ejs
+layout: layout.html
 ---
 
 # Hello world!
