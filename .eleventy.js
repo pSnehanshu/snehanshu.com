ab94f5f31a8a5b14227827f684e08813957c1c24
স্নেহাংশু ফুকন <hello@snehanshu.com>
2023-11-12 12:47:05 +0530

    minify html

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,26 @@
+const htmlmin = require("html-minifier");
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets/favicon.ico");
   eleventyConfig.addPassthroughCopy("./src/assets/media");
   eleventyConfig.addWatchTarget("_site/assets/main.css");
+
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    // Prior to Eleventy 2.0: use this.outputPath instead
+    if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        quoteCharacter: "'",
+        maxLineLength: 200,
+      });
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     dir: {
